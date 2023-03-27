42334280f12c3bc2fc1f6f1b10b369d8237e6356
Chescore <stevechesa@gmail.com>
2023-03-27 16:31:31 +0300

    Completed structure

--- client/src/components/Search/search.js
+++ client/src/components/Search/search.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
+import {Buffer} from 'buffer'
 
 const Search = () => {
     const [parameter, setParameter] = useState('brand')
 
     let [vehicles, setVehicles] = useState([])
+    let [images, setImages] = useState([])
 
     async function getVehicleList(){
         try{
             let response = await axios.get('http://localhost:5000/')
             setVehicles(response.data.vehicles)
+            setImages(response.data.images)
         }catch(err){
             console.log(err.response.data)
         }
@@ -23,10 +26,12 @@ const Search = () => {
 
     const showVehicles = () => {
         return vehicles.map((vehicle,i)=>{
+            let base64Image = Buffer.from(images[i].data).toString('base64')
             return(
                 <Link key={i} to={`/inventory/${vehicle._id}`}>
                     <div>{vehicle.brand}</div>
                     <div>{vehicle.year} {vehicle.brand} {vehicle.model}</div>
+                    <div><img src={`data:${images[i].contentType};base64,${base64Image}`} alt={vehicle.model}/></div>
                     <div>{vehicle.price}</div>
                 </Link>
             )
